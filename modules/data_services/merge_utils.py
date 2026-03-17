3ad730b8f17dc2a9ca5292e72f13cdb0fe45d5ce
damianlebiedz <damian.lebiedz@gmail.com>
2026-03-17 22:40:46 +0100

    Fix multi-period merging

--- modules/data_services/merge_utils.py
+++ modules/data_services/merge_utils.py
@@ -67,9 +67,6 @@ def stitch_strategy_results(
         if not res.exec_logger.empty:
             temp_exec_df = res.exec_logger.copy()
 
-            if "entry_equity" in temp_exec_df.columns:
-                temp_exec_df["entry_equity"] += offsets.get("total_net_pnl", 0.0)
-
             exec_dfs.append(temp_exec_df)
 
         if not df.empty:
